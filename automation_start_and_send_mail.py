6b1488ab7a79365834d25795f9e611a30664f8b4
thambathambz <35734446+thambathambz@users.noreply.github.com>
2024-02-22 23:33:44 +0530

    Update automation_start_and_send_mail.py

--- automation_start_and_send_mail.py
+++ automation_start_and_send_mail.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     # Start the Ec2
     ec2_client = boto3.client('ec2')
     waiter = ec2_client.get_waiter('instance_running')
-    instance_ids = ['i-0c094300ef6d27462', 'i-022ba6928b04714d5']
+    instance_ids = ['########', '############']
     response = ec2_client.start_instances(InstanceIds=instance_ids)
     waiter.wait(InstanceIds=instance_ids)
     print("instances are up and running fine")
